9cc2dc9468ea0805ded3da7a2ecb1cf418498c1a
Bradley Showen <BShowen@me.com>
2023-05-12 07:50:02 -0400

    Add query for retrieving all service routes

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -11,7 +11,73 @@ const roles = require("../helpers/roles");
 
 // Models
 const CustomerAccount = require("../models/CustomerAccount");
-const Technician = require("../models/Technician");
+
+/**
+ * Return an array of routes.
+ * Each element in the array is a route-object.
+ * A route object has a technician field and a customers field.
+ * The technician field is an object with _id, firstName, lastName.
+ * The customers field is an array of customer-accounts.
+ * [
+ *   {
+ *     technician: {_id, firstName, lastName},
+ *     customers: [{customerAccount}, {customerAccount}, ... ]
+ *   },
+ *   {
+ *     technician: {_id, firstName, lastName},
+ *     customers: [{customerAccount}, {customerAccount}, ... ]
+ *   },
+ *   ...
+ * ]
+ */
+router.get("/all", [
+  verifyJwt,
+  authorizeRoles(roles.ADMIN),
+  async (req, res, next) => {
+    try {
+      const companyId = new mongoose.Types.ObjectId(req.token.c_id);
+      const routes = await CustomerAccount.aggregate([
+        { $match: { companyId } },
+        {
+          $lookup: {
+            from: "technicians",
+            localField: "technicianId",
+            foreignField: "_id",
+            as: "technician",
+          },
+        },
+        {
+          $group: {
+            _id: {
+              technicianId: { $arrayElemAt: ["$technician._id", 0] },
+              firstName: { $arrayElemAt: ["$technician.firstName", 0] },
+              lastName: { $arrayElemAt: ["$technician.lastName", 0] },
+            },
+            customers: { $push: "$$ROOT" },
+            count: { $sum: 1 },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            technician: {
+              _id: "$_id.technicianId",
+              firstName: "$_id.firstName",
+              lastName: "$_id.lastName",
+            },
+            customers: 1,
+            count: 1,
+          },
+        },
+      ]);
+      return res.status(200).json(apiResponse({ data: { routes } }));
+    } catch (error) {
+      console.log(error);
+      res.status(400);
+      next(error);
+    }
+  },
+]);
 
 /**
  * Get the service route for a particular technician.
