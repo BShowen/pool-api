2b00b5a79a1150b10b9c8e5830698aa8e4fc0498
Bradley Showen <BShowen@me.com>
2023-05-18 20:27:43 -0400

    Implement create new customer account

--- src/models/CustomerAccount/customerAccount.resolvers.js
+++ src/models/CustomerAccount/customerAccount.resolvers.js
@@ -7,6 +7,30 @@ export default {
       return results;
     },
   },
+  Mutation: {
+    createNewCustomerAccount: async (
+      parent,
+      { customerAccountInput },
+      { user, models },
+      info
+    ) => {
+      user.authenticateAndAuthorize({ role: "MANAGER" });
+
+      const { CustomerAccount } = models;
+
+      customerAccountInput.companyId = user.c_id;
+
+      // When this was a rest API I had to parse the accountOwners because it
+      // was sent as a string.
+      // customerAccountInput.accountOwners = JSON.parse(
+      //   customerAccountInput.accountOwners || "[]"
+      // );
+
+      const newCustomerAccount = new CustomerAccount(customerAccountInput);
+      const savedCustomerAccount = await newCustomerAccount.save();
+      return savedCustomerAccount;
+    },
+  },
   CustomerAccount: {
     technician: async (parent, args, context, info) => {
       if (!parent.technicianId) {
