c7507f582e446feb2a737e047cbb1262ef36d66a
Bradley Showen <BShowen@me.com>
2023-05-18 20:49:58 -0400

    Implement get customer account query

--- src/models/CustomerAccount/customerAccount.resolvers.js
+++ src/models/CustomerAccount/customerAccount.resolvers.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 export default {
   Query: {
     customerAccountList: async (parent, args, context, info) => {
@@ -5,6 +7,22 @@ export default {
       user.authenticateAndAuthorize({ role: "MANAGER" });
       const results = await context.models.CustomerAccount.find();
       return results;
+    },
+    customerAccount: async (paren, { id }, { user, models }, info) => {
+      user.authenticateAndAuthorize({ role: "TECH" });
+      const { CustomerAccount } = models;
+      const companyId = new mongoose.Types.ObjectId(user.c_id);
+      const customerId = new mongoose.Types.ObjectId(id);
+      const customerAccount = await CustomerAccount.findOne(
+        { companyId: companyId, _id: customerId },
+        "-password"
+      );
+      if (!customerAccount) {
+        // A customer matching that id in this company wasn't found.
+        return null;
+      }
+
+      return customerAccount;
     },
   },
   Mutation: {
