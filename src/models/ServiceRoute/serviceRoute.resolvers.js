05096629888395e41bc4e4839b68b49b668234f1
Bradley Showen <BShowen@me.com>
2023-05-21 17:58:18 -0400

    Implement getGroupedServiceRoute query

--- src/models/ServiceRoute/serviceRoute.resolvers.js
+++ src/models/ServiceRoute/serviceRoute.resolvers.js
@@ -1,12 +1,45 @@
+import mongoose from "mongoose";
 export default {
   Query: {
-    serviceRouteList: async (parent, args, context, info) => {
+    getServiceRouteList: async (parent, args, context, info) => {
       const { user } = context;
       user.authenticateAndAuthorize({ role: "MANAGER" });
       const results = await context.models.Technician.find();
       return results.map((tech) => {
         return { technician: tech };
       });
+    },
+    getGroupedServiceRoute: async (parent, args, context, info) => {
+      const { user, models } = context;
+      const { id: technicianId } = args;
+
+      user.authenticateAndAuthorize({ role: "TECH" });
+
+      const routes = await models.CustomerAccount.aggregate([
+        {
+          $match: {
+            technicianId: new mongoose.Types.ObjectId(technicianId),
+            companyId: new mongoose.Types.ObjectId(user.c_id),
+          },
+        },
+        {
+          $group: {
+            _id: "$serviceDay",
+            customers: { $push: "$$ROOT" },
+            total: { $sum: "$price" },
+            count: { $sum: 1 },
+          },
+        },
+        {
+          $project: {
+            serviceDay: "$_id",
+            customers: 1,
+            total: 1,
+            count: 1,
+          },
+        },
+      ]);
+      return routes;
     },
   },
   ServiceRoute: {
